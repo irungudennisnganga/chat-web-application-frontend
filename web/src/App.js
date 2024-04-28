36482d6e4c548ef26a612500c665571090cc6e1b
+ <+>
2024-04-28 10:20:35 +0300

    add svg

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,61 @@
+import React, { useRef, useState } from 'react';
 import './App.css';
+import search from './Assets/search.svg';
+import camera from './Assets/camera.svg';
+import threeDots from './Assets/three-dots-.svg';
 
 function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [showCaptureButton, setShowCaptureButton] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const openCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const video = videoRef.current;
+      video.srcObject = stream;
+      video.autoplay = true;
+      setShowCaptureButton(true); // Show the capture button
+    } catch (error) {
+      // Add a notification for the user
+      console.error('Error accessing the camera: ', error);
+    }
+  };
+
+  const captureImage = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    // Set canvas dimensions to match video stream
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    // Draw current frame from video onto canvas
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Convert canvas data to image data URL
+    const imageDataURL = canvas.toDataURL('image/png');
+
+    // Set the captured image in state
+    setCapturedImage(imageDataURL);
+  };
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <div className="App bg-green-500">
+      <h1 className="text-3l font-bold padding-top: 0.25rem text-gray-200">CHAT COMMUNITY</h1>
+      
+      <img className='search' src={search} alt='search'/>
+      <img className='camera' src={camera} alt='camera' onClick={openCamera} />
+      <img className='dots' src={threeDots} alt='three dots'/>
+
+      <video ref={videoRef} style={{ display: 'none' }} />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+
+      {showCaptureButton && <button className='capture' onClick={captureImage}>Capture</button>}
+      
+      {capturedImage && <img src={capturedImage} alt="captured" />}
     </div>
   );
 }
